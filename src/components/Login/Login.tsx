f9cff6d6bb527b98f9485302e33fb24e3aad4268
nami <warriorparovoz@gmail.com>
2023-05-13 22:02:50 +0300

    feat(Login) implement login with API

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,20 +1,31 @@
+import { useState } from 'react'
 import Button from '../Button/Button'
 import { ButtonColors } from '../../types/ButtonColors'
+import axios from 'axios'
 import './Login.css'
 
 const Login = () => {
+  const [id, setId] = useState('')
+  const [token, setToken] = useState('')
+
+  const login = () => {
+    axios.get(`https://api.green-api.com/waInstance${id}/getStateInstance/${token}`)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err))
+  }
   return (
     <div className="login">
       <div className="login__field">
         <label className="login__label" htmlFor="id">IdInstance</label>
-        <input className="login__input" id="id" type="text" />
+        <input className="login__input" id="id" value={ id } onChange={ (e) => setId(e.target.value) } type="text" />
       </div>
       <div className="login__field">
         <label className="login__label" htmlFor="api">ApiTokenInstance</label>
-        <input className="login__input" id="api" type="text" />
+        <input className="login__input" id="api" value={ token } onChange={ (e) => setToken(e.target.value) } type="text" />
       </div>
       <Button  
         color={ ButtonColors.primaryOutline }
+        onButtonClick={ login }
       >
         Войти
       </Button>

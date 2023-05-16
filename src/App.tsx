f89fceff00c81da4b66f7e22cdbd71e6a9b0b00e
nami <warriorparovoz@gmail.com>
2023-05-16 20:47:46 +0300

    feat(App): realise send message

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Login from './components/Login/Login'
 import { userDataI } from './types/UserDataI'
+import Input from './Input/Input'
 import Welcome from './pages/Welcome/Welcome'
 import IconButtonList from './components/IconButtonList/IconButtonList'
 import Contacts from './components/Contacts/Contacts'
@@ -18,6 +19,7 @@ function App() {
     token: ''
   })
   const [activeChat, setActiveChat] = useState('')
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
     if (userData.id !== '') {
@@ -26,10 +28,20 @@ function App() {
     }
   }, [userData])
 
+  const sendMessage = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (message) {
+      axios.post(`https://api.green-api.com/waInstance${userData.id}/SendMessage/${userData.token}`, {
+        'chatId': `${activeChat}@c.us`,
+        'message': message
+      })
+    }
+  }
+
   return (
     <>
       <div className='container'>
-        {/* {userData.id ? */}
+        {userData.id ?
           <>
             <div className="sidebar">
               <div className="sidebar__header header">
@@ -37,18 +49,34 @@ function App() {
                 <IconButtonList icons={ [GroupImg, StatusImg, ChatImg, MenuImg] }/>
               </div>
               <Contacts
-                onContactSelect={ (contact) => setActiveChat(contact) }
+                onContactSelect={ (contact) => {
+                  setActiveChat(contact)
+                  setMessage('')
+                } }
               />
             </div>
             <div className="content">
               { activeChat 
-                ? <div></div>
+                ? 
+                <div className="chat">
+                  <form 
+                    className="chat__send"
+                    onSubmit={ sendMessage }  
+                  >
+                    <Input
+                      value={ message }
+                      onInputChange={ (message) => setMessage(message) }
+                    >
+                      Введите сообщение
+                    </Input>
+                  </form>
+                </div>
                 : <Welcome />
               }
             </div>
           </>
-          {/* : <Login onLogin={ (data: userDataI) => setUserData({...userData, ...data}) } />
-        } */}
+          : <Login onLogin={ (data: userDataI) => setUserData({...userData, ...data}) } />
+        }
       </div>
     </>
   )
